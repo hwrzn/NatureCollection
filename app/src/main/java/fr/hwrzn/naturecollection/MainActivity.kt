2aa661c65090f086b75d9cf97d3c9e5097707e57
hwrzn <hubert.wrzn@gmail.com>
2024-02-06 00:19:08 +0100

    première activité + résolution bug

--- app/src/main/java/fr/hwrzn/naturecollection/MainActivity.kt
+++ app/src/main/java/fr/hwrzn/naturecollection/MainActivity.kt
@@ -2,10 +2,17 @@ package fr.hwrzn.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.hwrzn.naturecollection.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //injection de HomeFragment dans notre FrameLayout (fragment_container)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
