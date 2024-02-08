499114c8cd907390a5494ec67a70b3a0a9018166
hwrzn <hubert.wrzn@gmail.com>
2024-02-08 21:19:38 +0100

    barre de navigation

--- app/src/main/java/fr/hwrzn/naturecollection/MainActivity.kt
+++ app/src/main/java/fr/hwrzn/naturecollection/MainActivity.kt
@@ -2,26 +2,57 @@ package fr.hwrzn.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import fr.hwrzn.naturecollection.fragments.AddPlantFragment
 import fr.hwrzn.naturecollection.fragments.CollectionFragment
 import fr.hwrzn.naturecollection.fragments.HomeFragment
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadFragment(HomeFragment(this), R.string.home_page_title)
+
+        //importer la BottomNavigationView
+        val navigationView = findViewById<BottomNavigationView>(R.id.navigation_view)
+        navigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId) // équivalent du switch en java
+            {
+                R.id.home_page -> {
+                    loadFragment(HomeFragment(this), R.string.home_page_title)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.collection_page -> {
+                    loadFragment(CollectionFragment(this), R.string.collection_page_title)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.add_plant_page -> {
+                    loadFragment(AddPlantFragment(this), R.string.add_plant_page_title)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment, string: Int) {
         //charger notre PlantRepository
         val repo = PlantRepository()
 
+        //actualiser le titre de la page
+        findViewById<TextView>(R.id.page_title).text = resources.getString(string)
+
+        //mettre à jour la liste de plantes
         repo.updateData{
-            //injection de HomeFragment dans notre FrameLayout (fragment_container)
+            //injection du fragment dans notre FrameLayout (fragment_container)
             val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, HomeFragment(this))
+            transaction.replace(R.id.fragment_container, fragment)
             transaction.addToBackStack(null)
             transaction.commit()
         }
-
-
     }
 }
